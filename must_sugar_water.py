1f654a7be181273ec127bb74149aabe6c3ae2ea3
Dimdd <108973368+Dimdd@users.noreply.github.com>
2022-07-26 14:49:49 +0300

    Commit 1

--- must_sugar_water.py
+++ must_sugar_water.py
@@ -1,13 +1,47 @@
 print("Fruit kg")
 kg_fruit=input()
+print("Fruit type")
+type_fruit=input()
 
 while(kg_fruit!="end"):
     fruit_quantity=float(kg_fruit)
-    water_quantity = fruit_quantity * 1.7  # it can vary between 1.2-2ltr per kg fruit depends of the fruit
-    sugar_quantity = fruit_quantity / 2  # it can vary between 0.1-1kg per kg fruit depends on the sugar content  of the fruit
-    print(f"Water ltr  {water_quantity}")
-    print(f"Sugar kg  {sugar_quantity}")
+    if type_fruit=="grape":
+        water_quantity = fruit_quantity * 1  # it can vary between 1.2-2ltr per kg fruit depends of the fruit
+        sugar_quantity = fruit_quantity / 1  # it can vary between 0.1-1kg per kg fruit depends on the sugar content  of the fruit
+        print(f"Water   {water_quantity} ltr")
+        print(f"Sugar   {sugar_quantity} kg")
+    elif type_fruit=="plum":
+        water_quantity = fruit_quantity * 1.7  # it can vary between 1.2-2ltr per kg fruit depends of the fruit
+        sugar_quantity = fruit_quantity / 2  # it can vary between 0.1-1kg per kg fruit depends on the sugar content  of the fruit
+        print(f"Water   {water_quantity} ltr")
+        print(f"Sugar   {sugar_quantity} kg")
+    elif type_fruit=="apricot":
+        water_quantity = fruit_quantity * 1.7  # it can vary between 1.2-2ltr per kg fruit depends of the fruit
+        sugar_quantity = fruit_quantity / 2  # it can vary between 0.1-1kg per kg fruit depends on the sugar content  of the fruit
+        print(f"Water   {water_quantity} ltr")
+        print(f"Sugar   {sugar_quantity} kg")
+    elif type_fruit=="fig":
+        water_quantity = fruit_quantity * 2  # it can vary between 1.2-2ltr per kg fruit depends of the fruit
+        sugar_quantity = fruit_quantity / 2.2  # it can vary between 0.1-1kg per kg fruit depends on the sugar content  of the fruit
+        print(f"Water   {water_quantity} ltr")
+        print(f"Sugar   {sugar_quantity} kg")
+    elif type_fruit == "mulberry":
+        water_quantity = fruit_quantity * 1.7  # it can vary between 1.2-2ltr per kg fruit depends of the fruit
+        sugar_quantity = fruit_quantity / 2  # it can vary between 0.1-1kg per kg fruit depends on the sugar content  of the fruit
+        print(f"Water   {water_quantity} ltr")
+        print(f"Sugar   {sugar_quantity} kg")
+    elif type_fruit == "pear":
+        water_quantity = fruit_quantity * 1.7  # it can vary between 1.2-2ltr per kg fruit depends of the fruit
+        sugar_quantity = fruit_quantity / 2  # it can vary between 0.1-1kg per kg fruit depends on the sugar content  of the fruit
+        print(f"Water   {water_quantity} ltr")
+        print(f"Sugar   {sugar_quantity} kg")
+    else:
+        water_quantity = fruit_quantity * 1.7  # it can vary between 1.2-2ltr per kg fruit depends of the fruit
+        sugar_quantity = fruit_quantity / 2  # it can vary between 0.1-1kg per kg fruit depends on the sugar content  of the fruit
+        print(f"Water   {water_quantity} ltr")
+        print(f"Sugar   {sugar_quantity} kg")
     kg_fruit=input()
+
 
 
 print()
